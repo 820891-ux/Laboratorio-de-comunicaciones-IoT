9d727e2866fd2f80c7e30a09db4df48a270778e8
Carlos_820891 <820891@unizar.es>
2026-09-20 13:19:19 +0200

    Practica 1 tarea 5
    Implementación del maestro I2C en Arduino Nano, enviando periódicamente valores 1 y 0 al esclavo para controlar el encendido y apagado de un LED.

--- Tarea5/Codigo_esp/Codigo_esp.ino
+++ Tarea5/Codigo_esp/Codigo_esp.ino
@@ -1,30 +1,49 @@
+```cpp
 #include <Wire.h>
 
+// Pines utilizados para la comunicación I2C
 #define SDA_PIN 21
 #define SCL_PIN 20
+
+// Pin donde está conectado el LED
 #define LED_PIN 1
 
+
+// Función que se ejecuta cuando se recibe un dato por I2C
 void recibirDato(int bytes) {
 
+  // Leemos el dato recibido
   int dato = Wire.read();
 
+  // Si recibimos un 1, encendemos el LED
   if (dato == 1) {
     digitalWrite(LED_PIN, HIGH);
   }
 
+  // Si recibimos un 0, apagamos el LED
   if (dato == 0) {
     digitalWrite(LED_PIN, LOW);
   }
 }
 
+
 void setup() {
 
+  // Configuramos el pin del LED como salida
   pinMode(LED_PIN, OUTPUT);
 
+  // Iniciamos la comunicación I2C como esclavo
+  // Dirección 8, pines SDA y SCL y frecuencia de 100 kHz
   Wire.begin(8, SDA_PIN, SCL_PIN, 100000);
 
+  // Indicamos qué función se ejecutará al recibir datos
   Wire.onReceive(recibirDato);
 }
 
+
 void loop() {
+
+  // No es necesario ejecutar nada aquí,
+  // ya que la recepción de datos se gestiona con onReceive
 }
+```
--- Tarea5/Codigo_nano/Codigo_nano.ino
+++ Tarea5/Codigo_nano/Codigo_nano.ino
@@ -1,20 +1,38 @@
+```cpp
 #include <Wire.h>
 
 void setup() {
+
+  // Iniciamos la comunicación I2C
+  // Al no indicar dirección, esta placa actúa como maestro
   Wire.begin();
 }
 
 void loop() {
 
+  // Iniciamos una transmisión al esclavo con dirección 8
   Wire.beginTransmission(8);
+
+  // Enviamos un 1 para indicar que se encienda el LED
   Wire.write(1);
+
+  // Finalizamos la transmisión
   Wire.endTransmission();
 
+  // Esperamos 1 segundo
   delay(1000);
 
+
+  // Volvemos a iniciar una transmisión al esclavo
   Wire.beginTransmission(8);
+
+  // Enviamos un 0 para indicar que se apague el LED
   Wire.write(0);
+
+  // Finalizamos la transmisión
   Wire.endTransmission();
 
+  // Esperamos 1 segundo
   delay(1000);
 }
+```
